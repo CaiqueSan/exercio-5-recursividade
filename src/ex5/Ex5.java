d17b49b9915f24993069f944374fcc4bdd30c850
CaiqueSan <105180821+CaiqueSan@users.noreply.github.com>
2023-02-28 20:12:13 -0300

    Update Ex5.java

--- src/ex5/Ex5.java
+++ src/ex5/Ex5.java
@@ -15,11 +15,11 @@ public class Ex5 {
 			System.out.println("Entrada limitada a 20");
 			return 0;
 			}
-		// condição de parada: se n for menor que 2 seus antecessores senão 1, e a soma da sequência será ele mesmo, então a função retorna n
+		// condiÃ§Ã£o de parada: se n for menor que 2 seus antecessores serÃ£o 1, e a soma da sequÃªncia serÃ¡ ele mesmo, entÃ£o a funÃ§Ã£o retorna n
 		if (n < 2) 
 			return n;
 		else 
-		// a função chama a si mesma duas vezes somando o retorno de ambar, e como parametro envia os dois antecessores do n atual, realizando
+		// a funÃ§Ã£o chama a si mesma duas vezes somando o retorno de ambas, e como parametro envia os dois antecessores do n atual, realizando
 		// a soma da sequencia.
 			return Ex5(n - 1) + Ex5(n - 2);
 	}
